dd7a08106308015161ccf9ff4004eb29e5b50d12
Vinny Sabatini <vincent.sabatini@kohls.com>
2018-07-25 19:14:40 -0500

    benchmark testing and concat

---
+++ iteration/iteration_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 7)
+	expected := "aaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%s' but got '%s'", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 6)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 3)
+	fmt.Println(repeated)
+	// Output: aaa
+}
