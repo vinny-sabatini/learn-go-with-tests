1b32573e0ed01a774c0dca088a335382866285d1
Vinny Sabatini <vincent.sabatini@kohls.com>
2018-07-28 13:53:49 -0500

    convert sumall to sumalltail

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,3 +8,15 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numberSet := range numbersToSum {
+		if len(numberSet) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numberSet[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("adding a collection of ints", func(t *testing.T) {
@@ -12,5 +15,28 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	})
+
+}
+
+func TestSumTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, want, got []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("basic test for the ability to sum up the tail of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4})
+		want := []int{2, 4}
+		checkSums(t, want, got)
+	})
+
+	t.Run("test adding an empty set", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2, 3, 4})
+		want := []int{0, 9}
+		checkSums(t, want, got)
 	})
 }
