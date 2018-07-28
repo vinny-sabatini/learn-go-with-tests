83aba87bcbab660c5e84d6a1882895827dcaba93
Vinny Sabatini <vincent.sabatini@kohls.com>
2018-07-28 16:46:53 -0500

    add triangle functionality, add granularity in area testing

--- structs/shapes.go
+++ structs/shapes.go
@@ -6,6 +6,23 @@ type Shape interface {
 	Area() float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 type Rectangle struct {
 	Length float64
 	Width  float64
@@ -13,14 +30,6 @@ type Rectangle struct {
 
 func (r Rectangle) Area() float64 {
 	return r.Length * r.Width
-}
-
-type Circle struct {
-	Radius float64
-}
-
-func (c Circle) Area() float64 {
-	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
