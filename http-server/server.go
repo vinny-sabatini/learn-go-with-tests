7ea822fd070146c62364a5d318762dc2b40d3c4b
Vinny Sabatini <vincent.sabatini@gmail.com>
2018-08-04 14:19:11 -0500

    initial server, get player, and in-place memory storage

---
+++ http-server/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
