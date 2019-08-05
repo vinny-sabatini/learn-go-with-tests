cbc71358e5255a29496a5bcc6b2b6b57247065ed
Vinny Sabatini <vincent.sabatini@gmail.com>
2019-08-04 22:40:33 -0500

    refactor to include cli and web app

---
+++ cli/league.go
@@ -0,0 +1,32 @@
+package poker
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+// League is a slice of players
+type League []Player
+
+// NewLeague initializes a league object
+func NewLeague(r io.Reader) ([]Player, error) {
+	var league []Player
+	err := json.NewDecoder(r).Decode(&league)
+	if err != nil {
+		err = fmt.Errorf("problem parsing league, %v", err)
+	}
+
+	return league, err
+}
+
+// Find retrieve a player if they exist in the league
+// Returns nil if player doesn't exist
+func (l League) Find(name string) *Player {
+	for i, p := range l {
+		if p.Name == name {
+			return &l[i]
+		}
+	}
+	return nil
+}
